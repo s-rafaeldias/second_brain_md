a0e22860086e17ed933ddfdaffe0f0dbaa12aa34
Rafael Dias <s.rafaeldias@protonmail.com>
2024-03-13 09:36:40 +0100

    initial commit

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class SecondBrain extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
@@ -97,26 +97,26 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: SecondBrain;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SecondBrain) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
